5135d4e8a41705a728567fa9e48358113c992c6f
defi <bl4ckh0l3@protonmail.ch>
2022-03-29 14:55:43 +0800

    init
    presale draft

---
+++ contracts/gameOn.sol
@@ -0,0 +1,289 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract pnDFIRE is ERC20, ERC20Burnable, Pausable, AccessControl {
+  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  constructor() ERC20("pnDFIRE", "pnDFIRE") {
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _grantRole(PAUSER_ROLE, msg.sender);
+    _grantRole(MINTER_ROLE, msg.sender);
+  }
+
+  function pause() public onlyRole(PAUSER_ROLE) {
+    _pause();
+  }
+
+  function unpause() public onlyRole(PAUSER_ROLE) {
+    _unpause();
+  }
+
+  function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    _mint(to, amount);
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override whenNotPaused {
+    super._beforeTokenTransfer(from, to, amount);
+  }
+}
+
+contract psDFIRE is ERC20, ERC20Burnable, Pausable, AccessControl {
+  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  constructor() ERC20("psDFIRE", "psDFIRE") {
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _grantRole(PAUSER_ROLE, msg.sender);
+    _grantRole(MINTER_ROLE, msg.sender);
+  }
+
+  function pause() public onlyRole(PAUSER_ROLE) {
+    _pause();
+  }
+
+  function unpause() public onlyRole(PAUSER_ROLE) {
+    _unpause();
+  }
+
+  function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    _mint(to, amount);
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override whenNotPaused {
+    super._beforeTokenTransfer(from, to, amount);
+  }
+}
+
+contract DeFIREGameOn is Ownable, ReentrancyGuard {
+  using SafeERC20 for ERC20;
+  using SafeERC20 for IERC20;
+  using SafeMath for uint256;
+
+  // -------------------------------------------------------------------------------
+
+  // treasury ops multisig
+  address public immutable multisigAddress;
+
+  // three tokens , three locking periods
+  address public immutable pnDFIREAddress;
+  address public immutable psDFIREAddress;
+
+  // // reputation token
+  address public immutable xFLAREAddress;
+
+  // invest token address (stable?18)
+  address public immutable stableTokenAddress;
+
+  IERC20 public stableToken;
+  IERC20 public flareToken;
+
+  pnDFIRE public pnDFIREToken;
+  psDFIRE public psDFIREToken;
+
+  // -------------------------------------------------------------------------------
+
+  struct InvestorStats {
+    uint256 depositedNative;
+    uint256 depositedUSD;
+    uint256 pnDFIRE;
+    uint256 psDFIRE;
+    string referral;
+  }
+
+  mapping(address => InvestorStats) public investorMatrix;
+
+  mapping(address => mapping(uint256 => uint256)) public phasedPledgeNative;
+  mapping(address => mapping(uint256 => uint256)) public phasedPledgeStable;
+
+  // ------------------------------------------------------------------------------- time
+
+  uint256 public createTime;
+
+  uint256 public sessionStartTime;
+  uint256 public sessionEndTime;
+
+  uint256 public phaseOneEndTime;
+  uint256 public phaseTwoEndTime;
+  uint256 public phaseThreeEndTime;
+
+  // -------------------------------------------------------------------------------
+
+  uint256 public minimumNative = 100;
+  uint256 public minimumStable = 100;
+  uint256 public phaseTime = 100;
+
+  // ------------------------------------------------------------------------------- events
+
+  event DepositedStable(uint256 indexed phase, address indexed wallet, uint256 amount);
+  event DepositedNative(uint256 indexed phase, address indexed wallet, uint256 amount);
+
+  // -------------------------------------------------------------------------------
+
+  constructor(
+    address _stableAddress,
+    address _xFLAREAddress,
+    address _pnDFIREAddress,
+    address _psDFIREAddress,
+    address _multisigAddress,
+    uint256 _sessionStartTime,
+    uint256 _phaseTime,
+    uint256 _minimumStable,
+    uint256 _minimumNative
+  ) {
+    require(_sessionStartTime > _phaseTime, "sessionStartTime must be greater than phase time");
+
+    require(_stableAddress != address(0), "Address cannot be 0x");
+    require(_xFLAREAddress != address(0), "Address cannot be 0x");
+    require(_pnDFIREAddress != address(0), "Address cannot be 0x");
+    require(_psDFIREAddress != address(0), "Address cannot be 0x");
+
+    require(_minimumNative > 1, "Minimum deposit should be higher than 1");
+    require(_minimumStable > 1, "Minimum deposit should be higher than 1");
+
+    stableToken = ERC20(_stableAddress);
+    flareToken = ERC20(_xFLAREAddress);
+    pnDFIREToken = pnDFIRE(_pnDFIREAddress);
+    psDFIREToken = psDFIRE(_psDFIREAddress);
+
+    stableTokenAddress = _stableAddress;
+    pnDFIREAddress = _pnDFIREAddress;
+    psDFIREAddress = _psDFIREAddress;
+
+    multisigAddress = _multisigAddress;
+    xFLAREAddress = _xFLAREAddress;
+
+    phaseTime = _phaseTime;
+
+    sessionStartTime = _sessionStartTime;
+    phaseOneEndTime = _sessionStartTime + (phaseTime * 1);
+    phaseTwoEndTime = _sessionStartTime + (phaseTime * 2);
+    phaseThreeEndTime = _sessionStartTime + (phaseTime * 3);
+    sessionEndTime = phaseThreeEndTime;
+
+    createTime = block.timestamp;
+  }
+
+  // ----------------------------------------------------------------------------------------
+
+  function seedPhase() public view returns (uint256) {
+    if (block.timestamp < sessionStartTime) {
+      return 0;
+    }
+    if (block.timestamp < phaseOneEndTime) {
+      return 1;
+    }
+    if (block.timestamp < phaseTwoEndTime) {
+      return 2;
+    }
+    if (block.timestamp < phaseThreeEndTime) {
+      return 3;
+    }
+    return 0;
+  }
+
+  function amountDepositedNative() public view onlyOwner returns (uint256) {
+    return address(this).balance;
+  }
+
+  function amountDepositedStable() public view onlyOwner returns (uint256) {
+    return stableToken.balanceOf(address(this));
+  }
+
+  // -------------------------------------------------------------------------------
+
+  function myFLARE() public view returns (uint256) {
+    return flareToken.balanceOf(msg.sender);
+  }
+
+  function myPNDFIRE() public view returns (uint256) {
+    return investorMatrix[msg.sender].pnDFIRE;
+  }
+
+  function myPSDFIRE() public view returns (uint256) {
+    return investorMatrix[msg.sender].psDFIRE;
+  }
+
+  // -------------------------------------------------------------------------------
+  function depositNative() external payable {
+    require(msg.sender != multisigAddress, "MultiSig address cannot deposit");
+    require(msg.value >= minimumNative, "Less than minimum deposit");
+    require(seedPhase() > 0, "Seed Round Closed");
+
+    if (seedPhase() < 3) {
+      require(flareToken.balanceOf(msg.sender) >= 3 ether, "Not enough FLARE to deposit");
+    }
+
+    InvestorStats storage investor = investorMatrix[msg.sender];
+
+    pnDFIREToken.mint(msg.sender, msg.value);
+
+    investor.depositedNative += msg.value;
+    investor.pnDFIRE += msg.value;
+
+    phasedPledgeNative[msg.sender][seedPhase()] += msg.value;
+
+    emit DepositedNative(seedPhase(), msg.sender, msg.value);
+  }
+
+  function depositStable(uint256 _amount, string memory _referral) external nonReentrant {
+    require(msg.sender != multisigAddress, "MultiSig address cannot deposit");
+    require(stableToken.balanceOf(msg.sender) >= _amount, "Not enough USDC to deposit");
+    require(_amount >= minimumStable, "Less than minimum deposit");
+    require(seedPhase() > 0, "Seed Round Closed");
+
+    if (seedPhase() < 3) {
+      require(flareToken.balanceOf(msg.sender) >= 3 ether, "Not enough FLARE to deposit");
+    }
+
+    InvestorStats storage investor = investorMatrix[msg.sender];
+
+    stableToken.safeTransferFrom(msg.sender, address(this), _amount);
+
+    psDFIREToken.mint(msg.sender, _amount);
+
+    investor.depositedUSD += _amount;
+    investor.psDFIRE += _amount;
+    investor.referral = _referral;
+
+    phasedPledgeStable[msg.sender][seedPhase()] += _amount;
+
+    emit DepositedStable(seedPhase(), msg.sender, _amount);
+  }
+
+  // ----------------------------------------------------------------------------------------
+
+  function withdrawStablesToTreasury() public onlyOwner {
+    require(seedPhase() == 0, "Seed Round Not Closed Yet");
+    stableToken.safeTransfer(multisigAddress, stableToken.balanceOf(address(this)));
+  }
+
+  function withdrawNativeToTreasury() public payable onlyOwner {
+    require(seedPhase() == 0, "Seed Round Not Closed Yet");
+    payable(multisigAddress).transfer(address(this).balance);
+  }
+
+  function withdrawUnclaimedToTreasury(IERC20 _token) public onlyOwner {
+    require(seedPhase() == 0, "Seed Round Not Closed Yet");
+    _token.safeTransfer(multisigAddress, _token.balanceOf(address(this)));
+  }
+}
